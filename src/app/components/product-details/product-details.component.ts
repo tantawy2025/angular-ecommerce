0ae5dff57e501908cbb48e8707ddc94e732e21f6
tantawy <mtantawy2025@gmail.com>
2025-06-03 05:22:36 +0300

    add product details page

---
+++ src/app/components/product-details/product-details.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../../common/product';
+import { ProductService } from '../../services/product.service';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-product-details',
+  standalone: true,
+  imports: [CommonModule,RouterModule],
+  templateUrl: './product-details.component.html',
+  styleUrl: './product-details.component.scss'
+})
+export class ProductDetailsComponent implements OnInit {
+
+  product: Product = new Product();
+
+  constructor(private productService: ProductService,
+              private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(() => {
+      this.handleProductDetails();
+    })
+  }
+
+  handleProductDetails() {
+
+    // get the "id" param string. convert string to a number using the "+" symbol
+    const theProductId: number = +this.route.snapshot.paramMap.get('id')!;
+
+    this.productService.getProduct(theProductId).subscribe(
+      data => {
+        this.product = data;
+      }
+    )
+  }
+
+}
